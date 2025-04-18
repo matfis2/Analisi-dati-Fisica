7f0d79e01fe2cb71a29af7b175aa045bcd8b36c5
enricodc <121957581+matfis2@users.noreply.github.com>
2025-04-18 14:56:50 +0200

    regressionePesataPoisson.cpp

--- regressione pesata poisson.cpp
+++ regressione pesata poisson.cpp
@@ -4,61 +4,115 @@
 #include <cmath>
 #include <string>
 using namespace std;
+
+//prototipi
+void interpolazionePoisson(vector<double> x,vector<double> y, int cov);
+double DeviazioneStandardPosteriori(double a, double b, vector<double> x,vector<double> y);
+double CoefficienteCorrelazione(vector<double> x, vector<double> y);
+
 int main() {
 	string filename;
-    vector<double> x, y, sigmay;
+    vector<double> x, y;
 	double xval,yval,sigma;
-	int scelta;
+    double n1,n2,n3,n4;
+    int cov=0;
 	
-	cout<<"___________REGRESSIONI LINEARI PESATE______ "<<endl;
-    cout<<"Seleziona se i dati possiedono una particolare distribuzione di probabilita'': "<<endl;
-
-    cout<<"Opzioni: - inserisci 0 per inserire le incertezze delle y personalizzate        - Inserisci 1 per dati con distribuzione di Possion "<<endl;
-  	cin>>scelta;
-		
+	cout<<"___________REGRESSIONE LINEARE PESATA POISSONIANA RAGGI GAMMA______ "<<endl;
     //operazioni dal file e presa dati
 	cout << "Inserire il nome del file di input: ";
     cin >> filename;
     ifstream fin(filename);
+
     if(!fin){
-    	cout<<"Il file non è stato caricato correttamente"<<endl;
+    	cout<<"Il file non e' stato caricato correttamente"<<endl;
     	return -1;
 	}
-	
-	if(scelta==0){
-		while(fin>>xval>>yval>>sigma){
-		if(xval>=320&&xval<=335 || xval>=390&&xval<=405){
-			x.push_back(xval);
-    			y.push_back(yval);
-    			sigmay.push_back(sigma);	
-		}
-		}	
-	}
-	if(scelta==1){
-		while(fin>>xval>>yval){
-		if(xval>=320&&xval<=335 || xval>=390&&xval<=405){
-			x.push_back(xval);
-    			y.push_back(yval);
-		}		
-		}
-		for(int i=0;i<y.size();i++){
-			sigmay.push_back(sqrt(y.at(i)));
-		}
-	}
-   
-	    
-    //Analisi dati         
-	// Calcolo delle sommatorie utili
+    cout<<"Inserisci i due intervalli di valori delle X dove vuoi fare l'interpolazione:  "<<endl;
+    cout<<" Da ";
+    cin>>n1;
+    cout<<" a "; 
+    cin>>n2;
+    cout<<"mentre il secondo intervallo: ";
+    cout<<" Da ";
+    cin>>n3;
+    cout<<" a ";
+    cin>>n4;
+
+	while(fin>>xval>>yval){
+		if(xval>=n1 && xval<=n2 || xval>=n3 && xval<=n4){
+            x.push_back(xval);    		
+            y.push_back(yval);
+        }
+    }
+    cout<<"I due parametri a e b sono covarianti? ( inserisci 1 se si , mentre 0 se non lo sono ): "<<endl;
+    cin>>cov;
+    interpolazionePoisson(x,y,cov);
+
+    return 0;
+}
+
+
+double DeviazioneStandardPosteriori(double a, double b, vector<double> x,vector<double> y) { 
+    double N = x.size();
+    double somma_quadrati_residui = 0.0;
+    // Calcola la somma dei quadrati dei residui
+    for (size_t i = 0; i < N; ++i) {
+        double residuo = y[i] - a - b * x[i];
+        somma_quadrati_residui += residuo * residuo;
+    }
+    // Calcola sigma posteriori
+    return sqrt(somma_quadrati_residui / (N - 2));
+}
+
+double CoefficienteCorrelazione(vector<double> x, vector<double> y) {    
+    const  double N = x.size();
+    double somma_x = 0.0, somma_y = 0.0;
+    double somma_x2 = 0.0, somma_y2 = 0.0;
+    double somma_prodotti = 0.0;
+    
+    // Calcola le medie
+    for (int i = 0; i < N; i++) {
+        somma_x += x[i];
+        somma_y += y[i];
+    }
+    double media_x = somma_x / N;
+    double media_y = somma_y / N;
+    
+    // Calcola le somme necessarie per la formula
+    for (size_t i = 0; i < N; i++) {
+        double diff_x = x[i] - media_x;
+        double diff_y = y[i] - media_y;
+        
+        somma_prodotti += diff_x * diff_y;
+        somma_x2 += diff_x * diff_x;
+        somma_y2 += diff_y * diff_y;
+    }
+    
+    // Calcola il coefficiente di correlazione
+    double denominatore = sqrt(somma_x2 * somma_y2);
+
+    if (denominatore == 0.0) {
+        return 0.0;
+    }
+    return somma_prodotti / denominatore;
+}
+
+void interpolazionePoisson(vector<double> x,vector<double> y, int cov){
     double spesi = 0.0;   
     double sx = 0.0;   
     double sy = 0.0;  
     double sxx = 0.0;  
     double sxy = 0.0;  
-	
-	
+	vector<double> sigmay;
+
+    for(int i=0;i<y.size();i++){
+        sigmay.push_back(sqrt(y.at(i)));
+    }
+
+    //attenzione che nel calcolo del peso potrebbero esserci alcuni problemi se il dato della y è zero
 	//calcolo somme intermedie
     for (int i = 0; i < x.size(); i++) {
-        double w = 1.0 / (sigmay[i] * sigmay[i]); // peso = 1/s_i^2
+        double w = 1.0 / (sigmay.at(i) * sigmay.at(i)); // peso = 1/s_i^2
         spesi  += w; //sommatoria pesi 
         sx  += x.at(i) * w; 
         sy  += y.at(i)* w;
@@ -76,18 +130,22 @@ int main() {
     //Calcolo delle incertezze relative ai parametri   
     double sigma_a = sqrt(sxx/ delta);
     double sigma_b = sqrt(spesi/ delta);
+    double covarianza=0;
+    if(cov==1){
+        covarianza=-(sx)/delta;
+    }
+
 
     // Stampa dei risultati
-    cout << "=== RISULTATI DEL FIT LINEARE PESATO per una retta y=a+bx ===" << endl;
+    cout << "=== RISULTATI DEL FIT LINEARE PESATO DI TIPO POISSONIANO per una retta y=a+bx ===" << endl;
     cout << "a = " << a << endl;
     cout << "b = " << b << endl;
     cout << "sigma_a = " << sigma_a <<endl;
     cout << "sigma_b = " << sigma_b << endl;
-	
-    string scelta2;
-    cout<<"I due parametri a e b sono covarianti?: ( scrivi: si oppure no )  "<<endl;
-    cin>>scelta2;
+    cout << "sigma posteriori = "<<DeviazioneStandardPosteriori(a,b,x,y)<<endl;
+    cout <<"coefficiente di correlazione r = "<<CoefficienteCorrelazione(x,y)<<endl;
+    if(cov==1){
+        cout<<"I due parametri a e b sono covarianti con una covarianza pari a: "<<covarianza<<endl;
+    }
 
-    return 0;
 }
-
